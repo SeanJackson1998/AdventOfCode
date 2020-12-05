15d545e6e814c8ddf07f345bd4dbd0df0b7b974a
Sean Jackson <seanjackson@Seans-MacBook-Pro.local>
2020-12-05 11:56:48 +0000

    Tidied day 1

--- 2020/01.py
+++ 2020/01.py
@@ -11,13 +11,12 @@ for num in nums:
 mini = min(nums)
 found = False
 for num in nums:
-    if (num < 2020 - 2 * mini):
+    if num < 2020 - 2 * mini:  # if the number is < 2020 - 2 of the min value in the list
         for num1 in nums:
-            if num1 < 2020 - num - mini:
-                if 2020 - num - num1 in nums:
+            if num1 < 2020 - num - mini:  # if the number < 2020 - num and the min value
+                if 2020 - num - num1 in nums:  # if the result of the subtraction is in the list then they sum to 2020
                     print(num * num1 * (2020 - num - num1))
                     found = True
                     break
         if found:
             break
-
